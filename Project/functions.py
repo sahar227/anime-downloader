5d514700aff7d401aa5e529433da625a7f6559b5
sahar227 <sahar22798@gmail.com>
2019-01-04 19:26:05 +0200

    added report logs for episodes and save torrents in dedicated folder also updated gitignore

--- Project/functions.py
+++ Project/functions.py
@@ -1,18 +1,24 @@
+import datetime
 import os
 from requests import get
 
+dirname = os.path.dirname(__file__)
+date = time = datetime.datetime.today().strftime('%d-%m-%Y')
 def downloadAndOpen(url, file_name):
+    f = open(dirname + '/downloadLogs/' + date + '.txt', "a+")
     print("        downloading " + file_name + " - " + url)
     response = get(url)
-    print("test")
     retries = 0
     while response.status_code != 200:
         response = get(url)
         print("retry number" + ++retries)
-    with open(file_name, "wb") as file:
+    with open(dirname + "/torrents/" + file_name, "wb") as file:
         # write to file
         file.write(response.content)
+        print("        downloaded successfully")
+        f.write(file_name)
         # Open file
         os.startfile(file_name)
+    f.close()
 
 #downloadAndOpen("https://nyaa.si/view/1107218/torrent", "Boogiepop wa Warawanai (2019)01.torrent")
