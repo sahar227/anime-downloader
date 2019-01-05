5eb0817951b2238f8e537fe8cb03260c97a49b9c
sahar227 <sahar22798@gmail.com>
2019-01-05 14:27:40 +0200

    Added check for whether file was already downloaded by us Fixed crush when download fails some code refactoring gitIgnore will now ignore the download log files

--- Project/app.py
+++ Project/app.py
@@ -5,7 +5,6 @@ from selenium.webdriver.chrome.options import Options
 import time
 import functions as functions
 
-animeFile = open("C:\\Users\\Sahar\\Documents\\Projects\\visual studio 2017\\AnimeDL GUI\\AnimeDL GUI\\bin\\Debug\\animeList.txt","r")
 
 # Web driver configuration
 chrome_options = Options()
@@ -20,7 +19,7 @@ if showsPage.status_code != 200:
 #######
 print("entered shows page!")
 showsPageSoup = BeautifulSoup(showsPage.content, "html.parser")
-for anime in animeFile:
+for anime in functions.animeFile:
     animeName = anime.replace("\n", "")
     print("Scanning " + animeName + ":")
     link = showsPageSoup.find("a", title= animeName)
@@ -32,13 +31,14 @@ for anime in animeFile:
     print("    got " + animeName + " site")####################################################################deleteME
     p_element = driver.find_element_by_class_name("hs-shows")
     for element in p_element.find_elements_by_class_name("rls-info-container"):
+        fileName = animeName + " " + element.find_element_by_tag_name("strong").text + ".torrent"  # anime name + number.torrent
         # download episodes that were uploaded today
-        if element.find_element_by_class_name("rls-date").text == "Today":
+        if element.find_element_by_class_name("rls-date").text == "Today" and not functions.HasDownloaded(fileName):
             element.find_element_by_class_name("rls-date").click()
             time.sleep(5)
             url = element.find_elements_by_class_name("rls-link")[2].find_elements_by_class_name("dl-type")[1].find_element_by_tag_name("a").get_attribute("href")
-            fileName = animeName + element.find_element_by_tag_name("strong").text + ".torrent" #anime name + number
             functions.downloadAndOpen(url, fileName)
         else:
             break
 driver.close()
+functions.animeFile.close()
--- Project/functions.py
+++ Project/functions.py
@@ -2,23 +2,46 @@ import datetime
 import os
 from requests import get
 
-dirname = os.path.dirname(__file__)
+# Global variables
+OriginDirName = os.path.dirname(__file__)
+DownloadLogsDirName = OriginDirName + '/downloadLogs/'
+TorrentsDirName = OriginDirName + '/torrents/'
+animeFile = open("C:\\Users\\Sahar\\Documents\\Projects\\visual studio 2017\\AnimeDL GUI\\AnimeDL GUI\\bin\\Debug\\animeList.txt","r")
 date = time = datetime.datetime.today().strftime('%d-%m-%Y')
+
 def downloadAndOpen(url, file_name):
-    f = open(dirname + '/downloadLogs/' + date + '.txt', "a+")
+    logFile = open(DownloadLogsDirName + date + '.txt', "a+")
     print("        downloading " + file_name + " - " + url)
-    response = get(url)
-    retries = 0
-    while response.status_code != 200:
+    try:
         response = get(url)
-        print("retry number" + ++retries)
-    with open(dirname + "/torrents/" + file_name, "wb") as file:
+    except:
+        print("Failed to download, retrying")
+        downloadAndOpen(url, file_name)
+        return False
+
+    with open(TorrentsDirName + file_name, "wb") as file:
         # write to file
         file.write(response.content)
         print("        downloaded successfully")
-        f.write(file_name)
+        logFile.write(file_name + "\n")
         # Open file
-        os.startfile(file_name)
-    f.close()
+        os.startfile(TorrentsDirName + file_name)
+        print("opened")
+    logFile.close()
+
+# Checks if logs already registered this episode
+##### TODO: in the future, should be used on Json file!
+def HasDownloaded(episodeName):
+    try:
+        logFile = open(OriginDirName + '/downloadLogs/' + date + '.txt', "r")
+    except:# file doesn't exist
+        return False
+    for line in logFile:
+        if episodeName in line:
+            logFile.close()
+            print("file " + episodeName + " already downloaded")
+            return True
+    logFile.close()
+    return False
 
 #downloadAndOpen("https://nyaa.si/view/1107218/torrent", "Boogiepop wa Warawanai (2019)01.torrent")
